2a62f2da4982f9e7fb87b97299e74b0ac8aca523
Jonas Nikula <jonas.nikula@student.tut.fi>
2019-12-11 20:07:57 +0100

    Fix some obvious pep8 issues

--- E9/ex09.py
+++ E9/ex09.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import misc
 
-
 ### load data
 # A: data matrix of size M x N
 # (nx,ny): image dimensions (number of pixels in horizontal and vertical direction
-A, N, M, nx, ny = np.load('data/data.npy');
+A, N, M, nx, ny = np.load('data/data.npy')
+
 
 ### auxilliary function
-def convert2image(A,t):
-    return A[:,t].reshape(ny,nx);
+def convert2image(A, t):
+    return A[:, t].reshape(ny, nx)
 
 
 ### optimization problem:
@@ -27,9 +27,9 @@ def convert2image(A,t):
 ###############################################################################
 
 ### Proximal Gradient Descent
-maxiter = 10000;
-check = 10;       # print the objective value and the rank of $Y$ after $check$ iterations
-tol = 1e-6;       # break iterations if the residual $res$ drops below $tol$
+maxiter = 10000
+check = 10  # print the objective value and the rank of $Y$ after $check$ iterations
+tol = 1e-6  # break iterations if the residual $res$ drops below $tol$
 
 # initialize 
 ###############################################################################
@@ -41,13 +41,12 @@ tol = 1e-6;       # break iterations if the residual $res$ drops below $tol$
 ###############################################################################
 
 
-for iter in np.arange(0,maxiter):
-    
+for iter in np.arange(0, maxiter):
+
     ###############################################################################
     # TODO: implement the Proximal Gradient Step 
-    
-    ###############################################################################
 
+    ###############################################################################
 
     ###############################################################################
     # TODO: 
@@ -61,43 +60,36 @@ for iter in np.arange(0,maxiter):
     # - implement a suitable stopping criterion $res$<$tol$ for the algorithm
 
     if res < tol:
-        break;
+        break
     ###############################################################################
 
-
-    if iter%check == 0:
-        print('iter: %d, val: %f, rank: %d' % (iter, val, rk));
-
+    if iter % check == 0:
+        print('iter: %d, val: %f, rank: %d' % (iter, val, rk))
 
 # visualize for every 10th image the outliers and the low-rank part
-fig = plt.figure();
-NN = N/10.0;
-gx = int(np.ceil(np.sqrt(NN)));
-for i in np.arange(0,NN):
-    plt.subplot(gx,gx,i+1);
-    img = convert2image(X,int(10*i));
-    plt.imshow(img);
+fig = plt.figure()
+NN = N / 10.0
+gx = int(np.ceil(np.sqrt(NN)))
+for i in np.arange(0, NN):
+    plt.subplot(gx, gx, i + 1)
+    img = convert2image(X, int(10 * i))
+    plt.imshow(img)
 
-fig2 = plt.figure();
-gx = int(np.ceil(np.sqrt(NN)));
-for i in np.arange(0,NN):
-    plt.subplot(gx,gx,i+1);
-    img = convert2image(Y,int(10*i));
-    plt.imshow(img);
+fig2 = plt.figure()
+gx = int(np.ceil(np.sqrt(NN)))
+for i in np.arange(0, NN):
+    plt.subplot(gx, gx, i + 1)
+    img = convert2image(Y, int(10 * i))
+    plt.imshow(img)
 
-plt.show();
-
+plt.show()
 
 # write 3 outlier masks
-img = convert2image(X,10);
-misc.imsave('outlier0010.png', img);
-img = convert2image(X,50);
-misc.imsave('outlier0050.png', img);
-img = convert2image(X,99);
-misc.imsave('outlier0099.png', img);
-img = convert2image(Y,50);
-misc.imsave('background0050.png', img);
-
-
-
-
+img = convert2image(X, 10)
+misc.imsave('outlier0010.png', img)
+img = convert2image(X, 50)
+misc.imsave('outlier0050.png', img)
+img = convert2image(X, 99)
+misc.imsave('outlier0099.png', img)
+img = convert2image(Y, 50)
+misc.imsave('background0050.png', img)
