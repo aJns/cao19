0ac12ae374d4d7fa74c2571ea906aceaff25c541
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-01-08 11:38:57 +0100

    Format stuff

--- E10/ex10_01.py
+++ E10/ex10_01.py
@@ -1,9 +1,7 @@
-import time;
+import time
 import numpy as np
 
-
 import matplotlib.pyplot as plt
-import myimgtools
 from myimgtools import make_derivatives2D
 
 ### Solve c ############################################
@@ -21,75 +19,70 @@ from myimgtools import make_derivatives2D
 ###############################################################################
 
 ### load data
-filename_image = 'data/flower.png';
+filename_image = 'data/flower.png'
 img = plt.imread(filename_image)
-(ny,nx,nc) = np.shape(img);
-print ("image dim: ", np.shape(img));
+(ny, nx, nc) = np.shape(img)
+print("image dim: ", np.shape(img))
 
 ### constuct the cost matrix
-fg_color = np.array([0,0,1]);
-bg_color = np.array([0,0,0]);
+fg_color = np.array([0, 0, 1])
+bg_color = np.array([0, 0, 0])
 
-rho = 0.1; # regularization weight
-cost = np.zeros((ny,nx));
-ones = np.ones((ny,nx));
-for i in np.arange(0,3):
-    cost = cost + (img[:,:,i] - fg_color[i]*ones)**2 
-    cost = cost - (img[:,:,i] - bg_color[i]*ones)**2 
-c = np.reshape(rho*cost, nx*ny); # vectorize cost image into cost vector 
+rho = 0.1  # regularization weight
+cost = np.zeros((ny, nx))
+ones = np.ones((ny, nx))
+for i in np.arange(0, 3):
+    cost = cost + (img[:, :, i] - fg_color[i] * ones) ** 2
+    cost = cost - (img[:, :, i] - bg_color[i] * ones) ** 2
+c = np.reshape(rho * cost, nx * ny)  # vectorize cost image into cost vector
 
-maxiter = 500;
-check = 10;
+maxiter = 500
+check = 10
 
-tau0 = 2.0;
-tau = tau0/8.0;
-val_ = 1e15;
+tau0 = 2.0
+tau = tau0 / 8.0
+val_ = 1e15
 
-K = make_derivatives2D(ny, nx);
-u_kp1 = np.ones(nx*ny);
-u_k = u_kp1.copy();
+K = make_derivatives2D(ny, nx)
+u_kp1 = np.ones(nx * ny)
+u_k = u_kp1.copy()
 
+# fig1 = plt.figure(1);
+# plt.show(block=False);
+for iter in np.arange(0, maxiter):
 
-
-
-#fig1 = plt.figure(1);
-#plt.show(block=False);
-for iter in np.arange(0,maxiter):
-   
     # TODO:  Complete the following todos
-    u_k = u_kp1;
+    u_k = u_kp1
 
     # TODO: Change tau appropriately for subgradient descent
 
     # TODO: compute subgradient 
-    
+
     # TODO: compute objective value at u_k
-    
+
     # TODO: update step
 
     # TODO: projection step
-    
-    # print information
-    if (iter%check == 0):
 
+    # print information
+    if iter % check == 0:
         ## show current result
         # TODO: display current result after each 'check' iteration
-        plt.figure(1);
+        plt.figure(1)
         # TODO: Create 'img_u' by reshaping u_kp1 appropriately 
-        
+
         # TODO: Create a segmentation colored rgb image 'col_img' where if an element 
         # of u_kp1 is >0 then set the class to 1 else 0
         # TODO: And, then all pixels belonging to class 0 should be given blue color
         # and class 1 is denoted by red color (foreground red, background blue)
         # Note: First reshape u_kp1 appropriately.
 
-
         # TODO: Create subplots where you show cost, img_u and col_img side by side.
         # TODO: add titles and make plots nicer (add color bar if required)
 
         plt.pause(0.01)  # to visualize the changes
 
-        print ("iter: %d, tau: %f, val: %f" %(iter, tau, val));
+        print("iter: %d, tau: %f, val: %f" % (iter, tau, val))
 plt.close()
 
 ### evaluation (apply classifier to all pixels)
@@ -100,7 +93,7 @@ plt.close()
 # TODO construct overlay image 'convex combination of original image and 
 # segmentation (col_img)'; You may set the weight for 'col_img' to 0.45.
 
-overlay = #TODO
+overlay = ...  # TODO
 
 # TODO: add titles and make plots nicer (other color bar)
 
@@ -109,7 +102,6 @@ plt.figure()
 # TODO: Create subplots where you show cost, img_u and overlay side by side.
 # TODO: add titles and make plots nicer (add color bar if required)
 plt.show()
-
 
 # TODO: save the variable 'col_img' to 'segmentation.png'
 
@@ -124,11 +116,9 @@ plt.show()
 # and class 1 is denoted by red color (foreground red, background blue)
 
 # Start naive thresholding implementation
-naive_img  = np.zeros((ny, nx, 3)) # visualizes the naive segmentation
+naive_img = np.zeros((ny, nx, 3))  # visualizes the naive segmentation
 
 # TODO: Save the segmentation obtained to the variable 'naive_img'
 
 
 # TODO: save the variable 'naive_img' to 'naive_img.png'
-
-
