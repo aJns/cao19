8c92230ba9d28a8de09250a6a5a9a58418a4f884
Jonas Nikula <jonas.nikula@student.tut.fi>
2020-01-08 12:16:07 +0100

    Visualize stuff

--- E10/ex10_01.py
+++ E10/ex10_01.py
@@ -22,6 +22,7 @@ from myimgtools import make_derivatives2D
 filename_image = 'data/flower.png'
 img = plt.imread(filename_image)
 (ny, nx, nc) = np.shape(img)
+img_2d_shape = (ny, nx)
 print("image dim: ", np.shape(img))
 
 ### constuct the cost matrix
@@ -31,7 +32,7 @@ bg_color = np.array([0, 0, 0])
 rho = 0.1  # regularization weight
 cost = np.zeros((ny, nx))
 ones = np.ones((ny, nx))
-for i in np.arange(0, 3):
+for i in range(3):
     cost = cost + (img[:, :, i] - fg_color[i] * ones) ** 2
     cost = cost - (img[:, :, i] - bg_color[i] * ones) ** 2
 c = np.reshape(rho * cost, nx * ny)  # vectorize cost image into cost vector
@@ -47,6 +48,19 @@ K = make_derivatives2D(ny, nx)
 u_kp1 = np.ones(nx * ny)
 u_k = u_kp1.copy()
 
+
+def objective_function(u):
+    inner_product = np.dot(c, u)
+    norm = np.linalg.norm(K * u, ord=1)
+    return inner_product * norm
+
+cost_values = []
+
+plt.figure(1)
+ax1 = plt.subplot(131)
+ax2 = plt.subplot(132)
+ax3 = plt.subplot(133)
+
 # fig1 = plt.figure(1);
 # plt.show(block=False);
 for iter in np.arange(0, maxiter):
@@ -58,7 +72,9 @@ for iter in np.arange(0, maxiter):
 
     # TODO: compute subgradient 
 
-    # TODO: compute objective value at u_k
+    # DONE: compute objective value at u_k
+    val = objective_function(u_k)
+    cost_values.append(val)
 
     # TODO: update step
 
@@ -68,19 +84,33 @@ for iter in np.arange(0, maxiter):
     if iter % check == 0:
         ## show current result
         # TODO: display current result after each 'check' iteration
-        plt.figure(1)
-        # TODO: Create 'img_u' by reshaping u_kp1 appropriately 
+        # DONE: Create 'img_u' by reshaping u_kp1 appropriately
+        img_u = np.reshape(u_kp1, img_2d_shape)
 
-        # TODO: Create a segmentation colored rgb image 'col_img' where if an element 
+        # DONE: Create a segmentation colored rgb image 'col_img' where if an element
         # of u_kp1 is >0 then set the class to 1 else 0
-        # TODO: And, then all pixels belonging to class 0 should be given blue color
+        col_img2d = img_u.copy()
+        col_img2d[col_img2d > 0] = 1
+        col_img2d[col_img2d <= 0] = 1
+
+        # DONE: And, then all pixels belonging to class 0 should be given blue color
         # and class 1 is denoted by red color (foreground red, background blue)
         # Note: First reshape u_kp1 appropriately.
+        col_img_red = col_img2d.copy()
+        col_img_blue = np.logical_not(col_img2d.copy())
+        col_img_green = np.zeros(img_2d_shape)
+        col_img = np.stack([col_img_red, col_img_green, col_img_blue], axis=-1)
 
-        # TODO: Create subplots where you show cost, img_u and col_img side by side.
+        # DONE: Create subplots where you show cost, img_u and col_img side by side.
+        # Maybe gotta clear these each iteration?
+        ax1.plot(cost_values)
+        ax2.imshow(img_u)
+        ax3.imshow(col_img)
+
         # TODO: add titles and make plots nicer (add color bar if required)
 
         plt.pause(0.01)  # to visualize the changes
+        # plt.pause(10)  # to visualize the changes
 
         print("iter: %d, tau: %f, val: %f" % (iter, tau, val))
 plt.close()
